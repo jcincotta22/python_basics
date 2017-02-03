d1416c37f6a9cb0b9f7cadb6fdcd172cf71f6a4a
jcincotta22 <jcincotta22@gmail.com>
2017-02-03 17:20:45 -0500

    reverse array

--- reverse.py
+++ reverse.py
@@ -5,3 +5,11 @@ def reverse(text):
         output.append(text[x])
         x -= 1
     return ''.join(output)
+
+def reverse_array(array):
+    l = len(array)
+    output = []
+    for x in array:
+        output.append(array[l - 1])
+        l -= 1
+    return output
